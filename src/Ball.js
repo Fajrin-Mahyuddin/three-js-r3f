10ca013bdff98953d66fe0a2c6d65b167fd31684
fajrin <fajrin.telkom12@gmail.com>
2022-11-30 23:25:43 +0800

    bowling effect

--- src/Ball.js
+++ src/Ball.js
@@ -1,11 +1,54 @@
 import { RigidBody } from "@react-three/rapier"
+import { useKeyboardControls } from '@react-three/drei';
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 const Ball = () => {
+	const body = useRef()
+	const [subscribeKeys, getKeys] = useKeyboardControls()
+	useFrame((state, delta) => {
+		const { forward, backward, leftward, rightward } = getKeys()
+		console.log(forward);
+		const impulse = { x: 0, y: 0, z: 0 }
+		const torque = { x: 0, y: 0, z: 0 }
+		const impulseStrength = 70 * delta
+		const torqueStrength = 50 * delta
+
+		if (forward) {
+			impulse.z -= impulseStrength
+			torque.x -= torqueStrength
+		}
+
+		if (rightward) {
+			impulse.x += impulseStrength
+			torque.z -= torqueStrength
+		}
+
+		if (backward) {
+			impulse.z += impulseStrength
+			torque.x += torqueStrength
+		}
+
+		if (leftward) {
+			impulse.x -= impulseStrength
+			torque.z += torqueStrength
+		}
+
+		body.current.applyImpulse(impulse)
+		body.current.applyTorqueImpulse(torque)
+	})
+
 	return (
-		<RigidBody colliders="ball" position={[0, 1.3, 0]}>
-			<mesh castShadow position={[1, 1, 1]}>
-				<sphereGeometry args={[1.2, 10, 10]} />
-				<meshStandardMaterial color="mediumpurple" flatShading />
+		<RigidBody ref={body} colliders="ball"
+			position={[0, 1.3, 0]}
+			restitution={0.5}
+			friction={1}
+			linearDamping={0.5}
+			angularDamping={0.5}
+		>
+			<mesh castShadow position={[0, 1, 10]}>
+				<icosahedronGeometry args={[1.2, 10, 10]} />
+				<meshStandardMaterial color="white" flatShading />
 			</mesh>
 		</RigidBody>
 	)
--- src/Level.js
+++ src/Level.js
@@ -32,22 +32,77 @@ const BlockStart = ({ position = [0, 0, 0] }) => {
 		// obstacleSpinner.current.position.x = Math.sin(time * 2) * 5
 		// obstacleSpinner.current.position.y = Math.cos(time * 2) * 5
 		const rotation = new THREE.Quaternion()
-		rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
-		obstacleSpinner.current.setNextKinematicRotation(rotation)
+		// rotation.setFromEuler(new THREE.Euler(0, time * speed, 0))
+		// obstacleSpinner.current.setNextKinematicRotation(rotation)
 	})
 	return (
 		<>
 			{/* <group position={position}> */}
 			{/* <RigidBody type="fixed"> */}
-			<mesh geometry={boxGeometry} material={floorMaterial} scale={[10, 0.2, 10]} position={[0, 0, 0]} receiveShadow>
+			<mesh geometry={boxGeometry} material={floorMaterial} scale={[15, 0.2, 15]} position={[0, 0, 0]} receiveShadow>
 			</mesh>
 			{/* </RigidBody> */}
-			<RigidBody ref={obstacleSpinner} type="kinematicPosition" position={[0, 0.4, 0]} restitution={0.2} friction={0}>
+			{/* <RigidBody ref={obstacleSpinner} type="kinematicPosition" position={[0, 0.4, 0]} restitution={0.2} friction={0}>
 				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "orange" })} scale={[3.5, 0.5, 0.3]} castShadow position={[0, 0.3, 0]} />
+			</RigidBody> */}
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[1, 5, -7]} />
 			</RigidBody>
-			<RigidBody colliders="ball">
-				<mesh geometry={sphereGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[1, 10, 0]} />
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[3, 5, -7]} />
 			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[5, 5, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[-5, 5, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[-3, 5, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "purple" })} castShadow position={[-1, 5, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "salmon" })} castShadow position={[-4, 7, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "salmon" })} castShadow position={[-2, 7, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "salmon" })} castShadow position={[0, 7, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "salmon" })} castShadow position={[2, 7, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "salmon" })} castShadow position={[4, 7, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "yellow" })} castShadow position={[-3, 9, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "yellow" })} castShadow position={[-1, 9, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "yellow" })} castShadow position={[1, 9, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "yellow" })} castShadow position={[3, 9, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "black" })} castShadow position={[-2, 11, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "black" })} castShadow position={[0, 11, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "black" })} castShadow position={[2, 11, -7]} />
+			</RigidBody>
+			<RigidBody>
+				<mesh geometry={boxGeometry} material={new THREE.MeshStandardMaterial({ color: "red" })} castShadow position={[0, 13, -7]} />
+			</RigidBody>
+
 			{/* </group> */}
 		</>
 	)
@@ -58,42 +113,42 @@ const Bounds = () => {
 	return <>
 		<RigidBody type="fixed" restitution={0.2} friction={0}>
 			<mesh
-				position={[10.30, 1.60, 0]}
+				position={[15.30, 1.60, 0]}
 				geometry={boxGeometry}
 				material={wallMaterial}
-				scale={[0.3, 1.5, 10]}
+				scale={[0.3, 1.5, 15]}
 				castShadow
 				receiveShadow
 			/>
 			<mesh
-				position={[-10.30, 1.60, 0]}
+				position={[-15.30, 1.60, 0]}
 				geometry={boxGeometry}
 				material={wallMaterial}
-				scale={[0.3, 1.5, 10]}
+				scale={[0.3, 1.5, 15]}
 				castShadow
 				receiveShadow
 			/>
 			<mesh
-				position={[0, 1.60, -10]}
+				position={[0, 1.60, -15]}
 				geometry={boxGeometry}
 				material={wallMaterial}
-				scale={[0.3, 1.5, 10]}
+				scale={[0.3, 1.5, 15]}
 				castShadow
 				receiveShadow
 				rotation={[0, Math.PI * 0.5, 0]}
 			/>
 			<mesh
-				position={[0, 1.60, 10]}
+				position={[0, 1.60, 15]}
 				geometry={boxGeometry}
 				material={wallMaterial}
-				scale={[0.3, 1.5, 10]}
+				scale={[0.3, 1.5, 15]}
 				castShadow
 				receiveShadow
 				rotation={[0, Math.PI * 0.5, 0]}
 			/>
 			<CuboidCollider
 				position={[0, 0.1, 0]}
-				args={[10, 0.2, 10]}
+				args={[15, 0.2, 15]}
 			/>
 		</RigidBody>
 	</>
